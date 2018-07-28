38966b8cfb9d78da3e05132ac2fbf2b288630e7f
bonzaiferroni <giPloki90()>
2018-07-27 20:25:12 -0600

    add DivvyEnable

---
+++ Core/DivvyEnable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UI;
+
+namespace Divvy.Core
+{
+    public class DivvyEnable : DivvyInstantVisibility
+    {
+        private Graphic _graphic;
+
+        public Graphic Graphic
+        {
+            get
+            {
+                if (!_graphic) _graphic = GetComponent<Graphic>();
+                return _graphic;
+            }
+        }
+
+        protected override void Modify(bool isVisible)
+        {
+            Graphic.enabled = isVisible;
+        }
+    }
+}
---
+++ Core/DivvyInstantVisibility.cs
@@ -0,0 +1,28 @@
+﻿namespace Divvy.Core
+{
+    public abstract class DivvyInstantVisibility : DivvyVisibility
+    {
+        public override void Show()
+        {
+            SetVisibility(true);
+        }
+
+        public override void Hide()
+        {
+            SetVisibility(false);
+        }
+
+        public override void Toggle()
+        {
+            SetVisibility(!IsVisible);
+        }
+
+        public override void SetVisibility(bool isVisible)
+        {
+            Modify(isVisible);
+            VisibilityChangeHandler(isVisible);
+        }
+
+        protected abstract void Modify(bool isVisible);
+    }
+}
