19ab44ea5ae347f62aa3225a5e6662ecbd37dd52
bonzaiferroni <giPloki90()>
2018-07-27 19:30:18 -0600

    add fade graphic

---
+++ Core/DivvyFadeGraphic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Divvy.Core
+{
+    public class DivvyFadeGraphic : DivvyVisibility
+    {
+        
+        public Graphic Graphic { get; private set; }
+        
+        public override void Init()
+        {
+            base.Init();
+            Graphic = GetComponent<Graphic>();
+        }
+
+        protected override void Modify(float amount)
+        {
+            Graphic.color = new Color(Graphic.color.r, Graphic.color.g, Graphic.color.b, amount);
+        }
+    }
+}
